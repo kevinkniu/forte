8b9133006a2c54bf3145928168bd856252f7ee45
Zhiya Xu <xuesmy@gmail.com>
2022-06-26 11:25:08 -0700

    fixed socket connection error

--- pages/api/socket.io.js
+++ pages/api/socket.io.js
@@ -15,8 +15,14 @@ const io = new Server(server, {
     method: ['GET', 'POST'],
   },
 });
+
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
+
+  socket.on('send_messsage', (data) => {
+    console.log(data);
+    socket.broadcast.emit('receive_message', data);
+  });
 });
 
 server.listen(3001, () => {
