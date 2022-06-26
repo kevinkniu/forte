3b7d28f957f70c1edbaa51dc4dad7786e9695952
Zhiya Xu <xuesmy@gmail.com>
2022-06-26 10:00:21 -0700

    merge message change

--- pages/messages.jsx
+++ pages/messages.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
