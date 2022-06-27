25a24fbbdf769b99a7134f1ed8d1bf4d25ca8b69
John Ong <itsjohnong@gmail.com>
2022-06-26 18:11:59 -0700

    starting music page added data from spotify api

--- pages/music.jsx
+++ pages/music.jsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import { Grid, Typography } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import BottomNav from './components/BottomNav';
 import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from '../config';
@@ -13,9 +14,14 @@ export default function Home() {
   const sessionObj = getSession?.user;
   const searchRef = useRef();
 
+  const [tokenId, setTokenId] = useState('');
+  const [genres, setGenres] = useState([]);
+  const [playlist, setPlaylist] = useState([]);
+  const [tracks, setTracks] = useState([]);
+  const [track, setTrack] = useState([]);
+
   const onSearch = (e) => {
     e.preventDefault();
-    console.log(searchRef.current.value);
   };
 
   useEffect(() => {
@@ -32,10 +38,66 @@ export default function Home() {
       });
 
       const data = await result.json();
-      console.log('token:', data.access_token);
-      return data.access_token;
+
+      return setTokenId(data.access_token);
     };
     getToken();
+
+    const getGenres = async (token) => {
+      const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+      });
+
+      const data = await result.json();
+
+      setGenres(data.categories?.items);
+      return data.categories.items;
+    };
+    getGenres(tokenId);
+
+    const getPlaylistByGenre = async (token, genreId) => {
+      const limit = 10;
+
+      const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+      });
+
+      const data = await result.json();
+
+      setPlaylist(data.playlists?.items);
+      return data.playlists.items;
+    };
+    getPlaylistByGenre(tokenId, 'toplists');
+
+    const getTracks = async (token, tracksEndPoint) => {
+      const limit = 10;
+
+      const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+      });
+
+      const data = await result.json();
+
+      setTracks(data.items);
+      return data.items;
+    };
+    getTracks(tokenId, 'https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M/tracks');
+
+    const getTrack = async (token, trackEndPoint) => {
+      const result = await fetch(`${trackEndPoint}`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+      });
+
+      const data = await result.json();
+
+      setTrack(data);
+      return data;
+    };
+    getTrack(tokenId, 'https://api.spotify.com/v1/tracks/2KukL7UlQ8TdvpaA7bY3ZJ');
   }, []);
 
   return (
@@ -62,6 +124,23 @@ export default function Home() {
           />
           <button hidden type="submit" onClick={(e) => { onSearch(e); }}>Submit</button>
         </Box>
+        <Grid>
+          <Grid>
+            {genres.length && genres.map((genre) => (
+              <Grid>
+                <Typography variant="h4" gutterBottom component="div" sx={{ borderBottom: '1px solid black', display: 'inline-block', fontSize: '10rem' }}>{genre?.name}</Typography>
+                <Grid sx={{ display: 'flex', flexDirection: 'row', overflow: 'auto' }}>
+                  {playlist.length && playlist.map((item) => (
+                    <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-end', marginRight: '2rem' }}>
+                      <img src={`${item.images[0].url}`} style={{'width': '40rem'}}/>
+                      <Typography variant="h6" gutterBottom component="div" sx={{ fontSize: '5rem' }}>{item.name}</Typography>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
       </main>
 
       <BottomNav />
