6df8cb26e3c7001c5ffa5c64ab3da6b7b9cc355d
Xiao Han <outsidenewcastle@gmail.com>
2022-06-28 16:59:19 -0700

    Get artist top tracks data

--- pages/artists/[id].jsx
+++ pages/artists/[id].jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import getToken from '../api/spotify/getToken';
 import getArtist from '../api/spotify/getArtist';
+import getTopTracks from '../api/spotify/getTopTracks';
 
-export default function Artist({ artistProp }) {
+export default function Artist({ artistProp, topTracksProp }) {
   return (<h1>Hello Artist id</h1>);
 }
 
 export async function getServerSideProps({ query }) {
   const tokenProp = await getToken();
   const artistProp = await getArtist(tokenProp, query.id);
+  const topTracksProp = await getTopTracks(tokenProp, query.id);
 
   return {
-    props: { artistProp },
+    props: { artistProp, topTracksProp },
   };
 }
