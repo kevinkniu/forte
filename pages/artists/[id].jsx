dc263d17b16d5e87dba1ddf2a166709c17c2ee84
Xiao Han <outsidenewcastle@gmail.com>
2022-06-28 16:10:29 -0700

    Get data from api

--- pages/artists/[id].jsx
+++ pages/artists/[id].jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import getToken from '../api/spotify/getToken';
+import getArtist from '../api/spotify/getArtist';
 
-export default function Artist() {
-  return <h1>Hello Artist </h1>
+export default function Artist({ artistProp }) {
+  return (<h1>Hello Artist id</h1>);
+}
+
+export async function getServerSideProps({ query }) {
+  const tokenProp = await getToken();
+  const artistProp = await getArtist(tokenProp, query.id);
+
+  return {
+    props: { artistProp },
+  };
 }
