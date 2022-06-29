b663d4df7a2a9a231943fd24d71c6ec3115abb76
Zhiya Xu <xuesmy@gmail.com>
2022-06-28 23:43:24 -0700

    can add chats to room messages and fixed bugs

--- pages/api/messages/createRoom.js
+++ pages/api/messages/createRoom.js
@@ -7,8 +7,6 @@ export default async function handler(req, res) {
     messages,
     users,
   });
-  // const newRoomData = await newRoom;
-  console.log(id);
-  // console.log('NEW ROOM', newRoom.id);
+
   res.status(200).json(id);
 }
