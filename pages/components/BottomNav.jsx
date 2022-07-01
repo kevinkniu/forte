0981f1725d38ee9e98f38e9956855363b0109272
Kevin Niu <kevinkniu@gmail.com>
2022-06-30 21:46:24 -0700

    most stylizations done

--- pages/components/BottomNav.jsx
+++ pages/components/BottomNav.jsx
@@ -5,26 +5,15 @@ import HomeIcon from '@mui/icons-material/Home';
 import PeopleIcon from '@mui/icons-material/People';
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 import PersonIcon from '@mui/icons-material/Person';
-import { makeStyles } from '@material-ui/core/styles';
 import { styled } from '@mui/material/styles';
 import { AppContext } from '../_app';
 
-// 'linear-gradient(#E5DCFD, #B09AF7)'
-
-const useStyles = makeStyles({
-  root: {
-    background: '#FFFFFF',
-  },
-  selected: {
-    color: '#00A57A',
-  },
-});
-
 const NewBottomNavigationAction = styled(BottomNavigationAction)(`
-  color: '#C4C4C4';
+  MuiBottomNavigationAction-root {
+    color: '#FFFFFF';
+  }
   &.Mui-selected {
-    color: #5D43BF;
-    font-weight: 600;
+    color: #B09AF7;
   }
 `);
 
@@ -32,22 +21,20 @@ export default function BottomNav() {
   const { value, setValue } = useContext(AppContext);
   const { asPath } = Router.useRouter();
 
-  const classes = useStyles();
-
   return (
     <Box sx={asPath === '/' ? 'hidden' : { position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 1500, boxShadow: 20 }} elevation={3}>
       <BottomNavigation
-        className={classes.root}
         showLabels
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
+        sx={{ bgcolor: '#121212', height: 60 }}
       >
-        <NewBottomNavigationAction label="Home" icon={<HomeIcon />} onClick={() => { Router.push('/home'); }} />
-        <NewBottomNavigationAction label="Friends" icon={<PeopleIcon />} onClick={() => { Router.push('/friends'); }} />
-        <NewBottomNavigationAction label="Music" icon={<LibraryMusicIcon />} onClick={() => { Router.push('/music'); }} />
-        <NewBottomNavigationAction label="Me" icon={<PersonIcon />} onClick={() => { Router.push('/profile'); }} />
+        <NewBottomNavigationAction sx={{ color: '#FFFFFF' }} label="Home" icon={<HomeIcon />} onClick={() => { Router.push('/home'); }} />
+        <NewBottomNavigationAction sx={{ color: '#FFFFFF' }} label="Friends" icon={<PeopleIcon />} onClick={() => { Router.push('/friends'); }} />
+        <NewBottomNavigationAction sx={{ color: '#FFFFFF' }} label="Music" icon={<LibraryMusicIcon />} onClick={() => { Router.push('/music'); }} />
+        <NewBottomNavigationAction sx={{ color: '#FFFFFF' }} label="Me" icon={<PersonIcon />} onClick={() => { Router.push('/profile'); }} />
       </BottomNavigation>
     </Box>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { useSession, signIn, getProviders } from 'next-auth/react';
 import Router from 'next/router';
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Typography } from '@mui/material';
 
 export default function Login({ providers }) {
   const { status } = useSession();
@@ -22,13 +22,11 @@ export default function Login({ providers }) {
             direction="column"
             alignItems="center"
             justifyContent="center"
-            style={{ minHeight: '100vh' }}
+            sx={{ minHeight: '100vh' }}
           >
             <img src="/forte.png" alt="" />
-            <Box mt={2}>
-              <button type="submit" onClick={() => { signIn(providers.spotify.id, { callbackUrl: '/home' }); }}>
-                Sign In with Spotify
-              </button>
+            <Box mt={2} onClick={() => { signIn(providers.spotify.id, { callbackUrl: '/home' }); }}>
+              <Typography sx={{ fontWeight: 700 }}>Sign In with Spotify</Typography>
             </Box>
           </Grid>
         </main>
