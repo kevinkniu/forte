30916f14813c7fa4e3fac76274a4df7475a09bcf
Kevin Niu <kevinkniu@gmail.com>
2022-07-01 11:28:36 -0700

    minor track display safeguard

--- pages/components/TrackListEntry.jsx
+++ pages/components/TrackListEntry.jsx
@@ -11,12 +11,12 @@ export default function TrackListEntry({ track }) {
     <Link href={`/track/${track.id}`}>
       <ListItem button className={trackListStyles.trackListEntry}>
         <img
-          src={track.album.images[0].url}
+          src={track?.album?.images[0]?.url || ''}
           alt="album-cover"
         />
         <div className={trackListStyles.trackListEntryInfo}>
-          <Typography noWrap sx={{ fontWeight: '600' }}>{track.name.length > 30 ? `${track.name.slice(0, 25)}...` : track.name}</Typography>
-          <Typography component="span">{track.artists[0].name}</Typography>
+          <Typography noWrap sx={{ fontWeight: '600' }}>{track?.name?.length > 30 ? `${track.name.slice(0, 25)}...` : track?.name}</Typography>
+          <Typography component="span">{track?.artists[0]?.name}</Typography>
         </div>
         <div>
           <PlayCircleOutlineIcon sx={{ color: '#44566C' }} />
